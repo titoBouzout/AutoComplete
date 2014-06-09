20b1b86dec55a1221f8338f1d99bccd818158b76
Tito Bouzout <tito.bouzout@gmail.com>
2014-06-08 21:57:40 -0300

    Do not break other completions

--- all_views_completions.py
+++ all_views_completions.py
@@ -13,6 +13,9 @@ class AllAutocomplete(sublime_plugin.EventListener):
 
         words = set()
 
+        if is_exclude(view, locations):
+            return ([], 0)
+
         # current view
         words.update(view.extract_completions(prefix, locations[0]) if len(locations) else view.extract_completions(prefix))
 
@@ -22,7 +25,32 @@ class AllAutocomplete(sublime_plugin.EventListener):
         if debug and words:
             print(words)
             print(str(time() - start_time) + '\twords: '+str(len(words)))
-
-        return [(w, ) for w in words]
+        return ([(w, ) for w in words], 0)
 
 AllAutocomplete.words = {} # for future caching.. of closed views
+
+def is_exclude(view, locations = None):
+    return (view.file_name() and should_exclude(view.file_name())) or should_exclude(view.settings().get('syntax')) or ( locations and len(locations) and should_exclude(view.scope_name(locations[0])))
+
+def should_exclude(string):
+    return len([1 for exclusion in Pref.excluded_files_folders_syntaxes_scopes if exclusion in string.lower()])
+
+Pref = {}
+s = {}
+
+class Pref():
+    def load(self):
+        if debug:
+            print('-----------------')
+        Pref.excluded_files_folders_syntaxes_scopes = [string.strip().lower() for string in s.get('excluded_files_folders_syntaxes_scopes', []) if string]
+
+def plugin_loaded():
+    global Pref, s
+    s = sublime.load_settings('AllAutoComplete.sublime-settings')
+    Pref = Pref()
+    Pref.load()
+    s.clear_on_change('reload')
+    s.add_on_change('reload', lambda:Pref.load())
+
+if int(sublime.version()) < 3000:
+    plugin_loaded()
